353f38c71e113fa97bb70f0be7ffe9957344067a
Rajat <vprr.1994@gmail.com>
2024-09-05 12:18:20 +0530

    Add files via upload
    BookMyShow_LLD

---
+++ bookmyshownov22-master/src/main/java/com/scaler/bookmyshownov22/Bookmyshownov22Application.java
@@ -0,0 +1,27 @@
+package com.scaler.bookmyshownov22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
+
+@SpringBootApplication
+public class Bookmyshownov22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bookmyshownov22Application.class, args);
+
+
+//        Object obj1 = null;
+//        obj1.toString();
+//
+//        Optional<Object> obj2 = Optional.of(null);
+//        if (obj2.isPresent()) {
+//
+//        }
+
+    }
+
+}
+
+// Break till 10:36 PM
